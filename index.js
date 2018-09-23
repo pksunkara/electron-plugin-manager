647a6cbe06677046815e2c297cc44e1d1d062f56
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2018-09-24 00:38:01 +0200

    Added travis

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const list = require('./lib/list');
 const install = require('./lib/install');
 const uninstall = require('./lib/uninstall');
+const path = require('./lib/path');
 
 module.exports = {
   list,
   install,
   uninstall,
+  path,
 };
