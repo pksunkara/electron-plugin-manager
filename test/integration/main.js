a42ea7d6ea823f59e2afd9721eacc9fd95cbec01
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2019-06-25 08:49:13 +0200

    Upgrade test deps

--- test/integration/main.js
+++ test/integration/main.js
@@ -7,7 +7,13 @@ epm.manager(ipcMain);
 
 function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({ width: 800, height: 600 });
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
   // and load the index.html of the app.
   win.loadURL(format({
