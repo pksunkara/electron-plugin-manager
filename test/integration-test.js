7db19d66323814ffb530337486042d3d75364945
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2022-02-12 11:08:50 +0000

    Upgrade deps

--- test/integration-test.js
+++ test/integration-test.js
@@ -1,7 +1,6 @@
 /* eslint-disable newline-per-chained-call, arrow-body-style, consistent-return */
 const { assert } = require('chai');
-const electron = require('electron');
-const { Application } = require('spectron');
+const { _electron: electron } = require('playwright');
 const path = require('path');
 const fs = require('fs');
 const rimraf = require('rimraf');
@@ -9,100 +8,88 @@ const rimraf = require('rimraf');
 const plugins = path.join(__dirname, 'integration', 'epm', 'plugins');
 const pkg = path.join(plugins, 'is-number');
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 describe('Integrating into Electron', () => {
+  let app, page;
+
   before((done) => {
     rimraf(path.join(__dirname, 'integration', 'epm'), done);
   });
 
-  beforeEach(() => {
-    this.app = new Application({
-      path: electron,
-      args: [path.join(__dirname, 'integration', 'main.js')],
-    });
-
-    return this.app.start();
+  beforeEach(async () => {
+    app = await electron.launch({ args: ['test/integration/main.js'] });
+    page = await app.firstWindow();
   });
 
-  afterEach(() => {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
-    }
+  afterEach(async () => {
+    await app.close();
   });
 
   it('opens a window', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    return this.app.client.getWindowCount().then((count) => {
-      assert.equal(count, 1);
-    });
+    assert.lengthOf(await app.windows(), 1);
   });
 
   it('has the html loaded', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    return this.app.client.getTitle().then((title) => {
-      assert.equal(title, 'Electron Plugin Manager Integration Test');
-    });
+    assert.equal(
+      'Electron Plugin Manager Integration Test',
+      await page.title()
+    );
   });
 
   it('can install a package', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    await (await this.app.client.$('#btn-install')).click();
-    await this.app.client.pause(1000);
+    await page.click('#btn-install');
+    await sleep(1000);
 
-    return (await this.app.client.$('#value')).getText().then((value) => {
-      assert.isTrue(fs.existsSync(pkg));
-      assert.equal(value, `${JSON.stringify(pkg)}`);
-    });
+    const value = await page.innerText('#value');
+
+    assert.isTrue(fs.existsSync(pkg));
+    assert.equal(value, `${JSON.stringify(pkg)}`);
   });
 
   it('can list packages', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    await (await this.app.client.$('#btn-list')).click();
-    await this.app.client.pause(1000);
+    await page.click('#btn-list');
+    await sleep(1000);
 
-    return (await this.app.client.$('#value')).getText().then((value) => {
-      assert.equal(value, '["is-number"]');
-    });
+    const value = await page.innerText('#value');
+    assert.equal(value, '["is-number"]');
   });
 
-  it('can list packages', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    await (await this.app.client.$('#btn-list-with-versions')).click();
-    await this.app.client.pause(1000);
+  it('can list packages with versions', async () => {
+    await page.click('#btn-list-with-versions');
+    await sleep(1000);
 
-    return (await this.app.client.$('#value')).getText().then((value) => {
-      assert.equal(value, '["is-number@7.0.0"]');
-    });
+    const value = await page.innerText('#value');
+    assert.equal(value, '["is-number@7.0.0"]');
   });
 
   it('can load a package', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    await (await this.app.client.$('#btn-load')).click();
-    await this.app.client.pause(1000);
+    await page.click('#btn-load');
+    await sleep(1000);
 
-    return (await this.app.client.$('#value')).getText().then((value) => {
-      assert.equal(value, 'true');
-    });
+    const value = await page.innerText('#value');
+    assert.equal(value, 'true');
   });
 
   it('can unload a package', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    await (await this.app.client.$('#btn-unload')).click();
-    await this.app.client.pause(1000);
+    await page.click('#btn-unload');
+    await sleep(1000);
 
-    return (await this.app.client.$('#value')).getText().then((value) => {
-      assert.equal(value, 'false');
-    });
+    const value = await page.innerText('#value');
+    assert.equal(value, 'false');
   });
 
   it('can uninstall a packages', async () => {
-    await this.app.client.waitUntilWindowLoaded();
-    await (await this.app.client.$('#btn-uninstall')).click();
-    await this.app.client.pause(1000);
+    await page.click('#btn-uninstall');
+    await sleep(1000);
 
-    return (await this.app.client.$('#value')).getText().then((value) => {
-      assert.isFalse(fs.existsSync(pkg));
-      assert.isTrue(fs.existsSync(plugins));
-      assert.equal(value, 'null');
-    });
+    const value = await page.innerText('#value');
+    assert.isFalse(fs.existsSync(pkg));
+    assert.isTrue(fs.existsSync(plugins));
+    assert.equal(value, 'null');
   });
 });
